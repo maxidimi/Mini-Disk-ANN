e7197192afecf07b09d780305f9caf2b15707e44
maxidimi <sdi2100034@di.uoa.gr>
2024-10-14 13:11:27 +0300

    Complete the algorithm without greedy and pruning.

--- Part-1/src/vamana_al.cpp
+++ Part-1/src/vamana_al.cpp
@@ -9,11 +9,11 @@ Data medoid(Dataset P) {
     
     Data s = P.front();
 
-    int min_distance = 0;
+    double min_distance = 0;
 
     for (auto p : P) {
         // distance from p to all other points in P
-        int distance = 0;
+        double distance = 0;
         for (auto q : P) {
             distance += euclidean_distance(p, q);
         }
@@ -27,40 +27,61 @@ Data medoid(Dataset P) {
     return s;
 }
 
+// Vamana Indexing Algorithm
 Graph vamana_indexing(Dataset P, int a, int L, int R) {
+    /*s is the medoid of dataset P
+      i is the index given by the random permutation σ
+      p is the i-st point in dataset P
+      p_in_graph is the node of graph with data p
+    */
     Graph graph;
+    int n = P.size();
 
     // Initialize G to a random R-regular directed graph
     for (auto p : P) {
         Graph_Node node = create_graph_node(p);
         add_node_to_graph(graph, node);
+        // Add R random out-neighbours to each node
+        while (node->out_neighbours.size() < (size_t)R) {
+            int random_index = rand() % n;
+            Data rand_ind = getElementAtIndex(P, random_index);
+            add_edge_to_graph(node, find_node_in_graph(graph, rand_ind));
+        }
     }
 
     // Let s denote the medoid of dataset P
     Graph_Node s = find_node_in_graph(graph, medoid(P));
 
-    // Let σ denote a random permutation of 1...n
-    vector<int> sigma;
-    random_permutation(sigma, P);
+    // Let σ denote a random permutation of P
+    auto P_perm = random_permutation(n);
 
-    // For each point p in σ do
-    int i = 0;
-    for (auto s_i : sigma) {
-        // Let s_i denote the i-th point in σ and s_iter 
-        auto s_item = P.begin();
-        advance(s_item, s_i);
+    for (auto i : P_perm) {
+        // Node of graph with data p
+        auto p = getElementAtIndex(P, i);
+        auto p_in_graph = find_node_in_graph(graph, p);
 
-        Graph_Node s_iter = find_node_in_graph(graph, *s_item);
-        graph = robust_pruning(graph, s_iter->data, P, a, R);
+        // Run greedy_search, V are the visited nodes, L_res are the nearest neighbours
+        list<Graph_Node> V; 
+        //?auto L_res = greedy_search(graph, s, p, 1, L, V);
+
+        // Run robust_prune to update out-neighbours of s_i
+        //?robust_pruning(graph, p_in_graph, L_res, a, R);
 
         // For all points j in N_out(σ(i)) do
-        for (auto s_j : sigma) {
+        for (auto j : p_in_graph->out_neighbours) {
+            // |N_out(j) U {σ(i)}|
+            list<Graph_Node> N_out_j = j->out_neighbours;
+            N_out_j.push_back(p_in_graph);
+
             // If |N_out(σ(i)) U {σ(i)}| > R then
-                // Run robust_prune(j, N_out(j) U {σ(i)}, a, R)
-            // Else
-                // N_out(j) = N_out(j) U {σ(i)}
+            if (N_out_j.size() > (size_t)R) {
+                // Run robust_prune to update out-neighbours of j
+                //?robust_pruning(graph, j, N_out_j, a, R);
+            } else {
+                // Add σ(i) to out-neighbours of j
+                add_edge_to_graph(j, p_in_graph);
+            }
         }
-        
     }
     
     // Return the graph
