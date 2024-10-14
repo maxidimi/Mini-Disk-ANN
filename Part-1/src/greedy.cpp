21f8bdbb2cdf535f9f3c3d3778f8d7fe451aedc5
Giorgos Ragkos <ragkos7@gmail.com>
2024-10-14 14:32:17 +0300

    Start of greedy algorithm

--- Part-1/src/greedy.cpp
+++ Part-1/src/greedy.cpp
@@ -1,3 +1,14 @@
 #include "../include/header.h"
 
 // Greedy Algorithm [returning k-nearest aprx. points]
+
+Dataset greedy_search(Graph_Node s, Data q, int k, int L){
+    Dataset result;
+    list<Graph_Node> searching_list;
+    searching_list.push_back(s); //The first node of the graph
+    while(!searching_list.empty() && result.size() < k ){
+        //TODO
+    }
+
+
+}
