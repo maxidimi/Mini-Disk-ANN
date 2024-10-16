6156eea3edc92a124df0d141f0d56ab63f65ad8d
Giorgos Ragkos <ragkos7@gmail.com>
2024-10-16 20:48:18 +0300

    Unit testing for help functions

--- Part-1/testing/test.cpp
+++ Part-1/testing/test.cpp
@@ -44,9 +44,48 @@ void test_knn(void) {
     double min_distance = *min_element(distances.begin(), distances.end());*/
 }
 
+void test_euclidean_distance(void){
+    //Test for 1D
+    Data d1 = {20};
+    Data d2 = {10};
+    TEST_CHECK(fabs(euclidean_distance(d1, d2) - 10) < 1e-6);
+    
+    //Test for 2D
+    d1 = {2, 5};
+    d2 = {3, 4};
+    TEST_CHECK(fabs(euclidean_distance(d1, d2) - sqrt(2)) < 1e-6);
+
+    //Test for 3D
+    d1 = {3, 6, 9};
+    d2 = {2, 4, 6};
+    TEST_CHECK(fabs(euclidean_distance(d1, d2) - sqrt(14)) < 1e-6);
+
+    //Test for 4D
+    d1 = {3, 6, 9, 12};
+    d2 = {2, 4, 6, 8};
+    TEST_CHECK(fabs(euclidean_distance(d1, d2) - sqrt(30)) < 1e-6);
+
+}
+
+void test_random_permutation(void){
+    int n = 25;
+    list<int> perm = random_permutation(n);
+    TEST_CHECK(perm.size() == static_cast<std::list<int>::size_type>(n));
+
+    n = 10;
+    list<int> perm1 = random_permutation(n);
+    list<int> perm2 = random_permutation(n);
+    TEST_CHECK(perm1 != perm2);  
+    
+}
+
+
+
 TEST_LIST = {
     { "test_create_graph_node", test_create_graph_node },
     { "test_example", test_example },
     { "test_knn", test_knn },
+    {"test_euclidean_distance", test_euclidean_distance},
+    {"test_random_permutation", test_random_permutation},
     { 0 }
 };
