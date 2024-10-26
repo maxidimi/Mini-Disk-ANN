164a4dfe8295a700f904207fd2affe2ece6e8b14
Dimitris Dimochronis <97557277+maxidimi@users.noreply.github.com>
2024-10-26 18:06:09 +0300

    Update pruning.cpp

--- Part-1/src/pruning.cpp
+++ Part-1/src/pruning.cpp
@@ -7,23 +7,15 @@ Graph robust_pruning(Graph &G, Data &p, Dataset &V, double a, int R) {
         p is the data point in the graph from the input
     */
     // V <- (V U N_out(p)) \ {p} withouth duplicates
-    Graph_Node P = find_node_in_graph(G,p);
-    if (P == nullptr) {
-        cout << "Point not found in the graph\n";
-        return G;
-    }
-
-    Graph N_out_p(P->out_neighbours.begin(), P->out_neighbours.end());
-    auto iter = find(V.begin(), V.end(), p);
-    if(iter != V.end()){
-        V.erase(iter);
-    }
-
-    // V <- V U N_out(p) without duplicates
+    Graph_Node P = find_node_in_graph(G,p); Graph N_out_p = P->out_neighbours;
     for (const auto &i : N_out_p) {
         if (find(V.begin(), V.end(), i->data) == V.end()) {
             V.push_back(i->data);
         }
+    }
+    auto iter = find(V.begin(), V.end(), p);
+    if (iter != V.end()) {
+        V.erase(iter);
     }
     
     // N_out(p) <- {}
@@ -32,9 +24,8 @@ Graph robust_pruning(Graph &G, Data &p, Dataset &V, double a, int R) {
     // While V not empty
     while (!V.empty()) {
         // Find p* <- argmin_{p \in V} d(p,q)
-        Data p_ = V.front(); // p_ = p*
+        Data p_ = V.front();
         data_t min = euclidean_distance(p,p_);
-
         for (const auto &i : V) { // i = p'
             data_t dist = euclidean_distance(p,i);
             if (dist < min) {
@@ -43,13 +34,14 @@ Graph robust_pruning(Graph &G, Data &p, Dataset &V, double a, int R) {
             }
         }
 
-        // Find node p* in graph and add it to N_out(p)
+        // Find node p* in graph and add it to N_out(p) if not already present
         Graph_Node P_ = find_node_in_graph(G,p_);
         if (P_ == nullptr) {
             cout << "Point not found in the graph\n";
             return G;
         }
-        N_out_p.push_back(P_);
+        if (find(N_out_p.begin(), N_out_p.end(), P_) == N_out_p.end())
+            N_out_p.push_back(P_);
 
         // If |N_out(p)| = R, break
         if ((int)N_out_p.size() == R) break;
@@ -58,7 +50,7 @@ Graph robust_pruning(Graph &G, Data &p, Dataset &V, double a, int R) {
         Dataset V_dup = V;
         for (const auto &i : V_dup) { // i = p'
             // If a*d(p*, p') <= d(p,p')
-            if ((double)a*euclidean_distance(p_,i) <= euclidean_distance(p, i)) {
+            if ((data_t)a*euclidean_distance(p_,i) <= euclidean_distance(p, i)) {
                 // Remove p' from V
                 auto iter = find(V.begin(), V.end(), i);
                 if (iter != V.end()) {
@@ -68,7 +60,7 @@ Graph robust_pruning(Graph &G, Data &p, Dataset &V, double a, int R) {
         }
         
     }
-
+    
     // Add edges from p to N_out(p)
     P->out_neighbours.clear();
     for (const auto &i : N_out_p) {
