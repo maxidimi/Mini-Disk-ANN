30f4236d329511bb22d44da67bff4d1d143cf429
sdi2100152 <sdi2100152@di.uoa.gr>
2024-10-29 11:55:08 +0200

    reading .ivecs files

--- Part-1/src/data_forming.cpp
+++ Part-1/src/data_forming.cpp
@@ -48,7 +48,7 @@ vector<vector<float>> fvecs_read(char* file_name){
     fseek(file, 0, SEEK_END);
     // Calculate the number of vectors
     long veccount = ftell(file) / vecsizeof ;
-    cout<<vecsizeof/4<< "  " <<veccount<<endl;
+
     vector<vector<float>> arr;
     fseek(file,0, SEEK_SET);
     //We have a loop to read every vector
@@ -62,3 +62,31 @@ vector<vector<float>> fvecs_read(char* file_name){
     return arr;
 }
 
+
+vector<vector<int>> ivecs_read(char * file_name) {
+    auto *file = fopen(file_name, "r");
+    if (file == nullptr) {
+        cout << "Error, can not open the file";
+        return {};
+    }
+    //Reading the size of the vectors
+    int num;
+    fread(&num, sizeof(int), 1, file);
+    
+    int vecsizeof = 4 + num*4;
+    
+    fseek(file, 0, SEEK_END);
+    // Calculate the number of vectors
+    long veccount = ftell(file) / vecsizeof ;
+    vector<vector<int>> arr;
+    fseek(file,0, SEEK_SET);
+    //We have a loop to read every vector
+    for (int i = 0; i < veccount; i++) {
+        fseek(file, sizeof(int), SEEK_CUR);
+        vector<int> v(num);
+        fread(v.data(), sizeof(int), num, file);
+        arr.push_back(v);
+    }
+    fclose(file);
+    return arr;
+}
