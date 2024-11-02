3f3e9e48a46520f75abc356537b84124ed727025
Giorgos Ragkos <ragkos7@gmail.com>
2024-11-02 15:35:16 +0200

    final greedy update

--- Part-1/src/graph.cpp
+++ Part-1/src/graph.cpp
@@ -1,24 +1,21 @@
 #include "../include/header.h"
 
 // Creates a graph node
-Graph_Node create_graph_node(Data &data) {
+Graph_Node create_graph_node(const Data &data, int R) {
     Graph_Node node = new graph_node;
     node->data = data;
+    node->out_neighbours.reserve(R);
     return node;
 }
 
 // Adds a node to the graph
-void add_node_to_graph(Graph &graph, Graph_Node node) {
+void add_node_to_graph(Graph &graph, const Graph_Node &node) {
     graph.push_back(node);
 }
 
 // Adds an edge to the graph
-void add_edge_to_graph(Graph_Node from, Graph_Node to) {
-    // Check if the edge already exists
-    auto it = find(from->out_neighbours.begin(), from->out_neighbours.end(), to);
-    if (it == from->out_neighbours.end()) {
-        from->out_neighbours.push_back(to);
-    }
+void add_edge_to_graph(Graph_Node &from, const Graph_Node &to) {
+    from->out_neighbours.insert(to);
 }
 
 // Finds a node in the graph
@@ -33,7 +30,7 @@ Graph_Node find_node_in_graph(Graph &graph, const Data &data) {
 }
 
 // Prints the graph
-void print_graph(Graph &graph) {
+void print_graph(const Graph &graph) {
     cout << "Graph:\n\t";
     for (const auto &i : graph) {
         cout << "( ";
@@ -44,7 +41,7 @@ void print_graph(Graph &graph) {
 }
 
 // Prints the out neighbours of a node
-void print_out_neighbours(Graph_Node node) {
+void print_out_neighbours(const Graph_Node &node) {
     cout << "\nOut-neighbours of node: (";
     for (const auto &i : node->data) {
         cout << i << ", ";
--- Part-1/src/helper.cpp
+++ Part-1/src/helper.cpp
@@ -1,17 +1,22 @@
 #include "../include/header.h"
 
 // Computes the Euclidean distance between two data points
-data_t euclidean_distance(const Data &d1, const Data &d2) {
+euclidean_t euclidean_distance(const Data &d1, const Data &d2) {
     if (d1.size() != d2.size()) {
         cerr << "Data points have different dimensions\n";
         exit(1);
     }
-    size_t dim = d1.size(); data_t distance = 0.0; 
+
+    size_t dim = d1.size();
+    euclidean_t distance = 0.0L;
+
     for (size_t i = 0; i < dim; i++) {
         distance += (d2[i] - d1[i]) * (d2[i] - d1[i]);
     }
     
-    return sqrt(distance);
+    //distance = sqrt(distance);
+    
+    return distance;
 }
 
 // Generates a random permutation of integers from 0 to n-1
@@ -21,73 +26,34 @@ vector<int> random_permutation(int n) {
         perm[i] = i;
     }
 
-    shuffle(perm.begin(), perm.end(), mt19937(random_device()()));
+    random_shuffle(perm.begin(), perm.end());
 
     return perm;
 }
 
-// Returns the element at the given index in a list
-Data get_element_at_index(Dataset &mylist, size_t index) {
-    // Check if index is out of bounds
-    if (index >= mylist.size()) {
-        cerr << "Index is out of bounds\n";
-        exit(1);
-    }
-
-    return mylist[index];
-}
-
-// Changes the element at the given index in a list
-void change_element_at_index(Dataset &mylist, size_t index, Data &data) {
-    // Check if index is out of bounds
-    if (index >= mylist.size()) {
-        cerr << "Index is out of bounds\n";
-        exit(1);
-    }
-
-    mylist[index] = data;
-}
-
 // Given a graph node list, returns the corresponding dataset
-Dataset get_data(Graph &graph) {
-    Dataset data;
+Dataset get_data(const Graph &graph) {
+    Dataset data; data.reserve(graph.size());
     for (const auto &node : graph) {
         data.push_back(node->data);
     }
     return data;
 }
 
-// L\V operation, returns the elements in L that are not in V
-Graph L_m_V(Graph &L, Graph &V) {
-    Graph result;
-    for (const auto &node : L) {
-        if (find(V.begin(), V.end(), node) == V.end()) {
-            result.push_back(node);
-        }
-    }
-    
-    return result;
-}
-
-// Wrapper function to get the node in the graph with the given data
-Graph_Node get_node_at_index(Graph &graph, Dataset &P, int index) {
-    auto data = P[index];
-    
-    return find_node_in_graph(graph, data);
-}
-
 // Generates a random query of the given dimension
 Data random_query(int dim) {
-    Data query; srand((unsigned int)time(0));
+    Data query;
+
     for (int i = 0; i < dim; i++) {
         query.push_back((data_t)rand() / RAND_MAX);
     }
+
     return query;
 }
 
 // Generates a random dataset of the given size and dimension
 Dataset random_dataset(int n, int dim) {
-    srand((unsigned int)time(0));
+
     Dataset dataset;
     for (int i = 0; i < n; i++) {
         Data data;
@@ -101,7 +67,7 @@ Dataset random_dataset(int n, int dim) {
 
 // Prints the results of the Vamana indexing and Greedy search
 void print_results(const Dataset &dataset, const Data &query, const vector<Data> &expected_neighbors,\
-                     const Graph &result, const vector<pair<Data, data_t>> &distances) {
+                     const Graph &result, const vector<pair<Data, euclidean_t>> &distances) {
     // Print the distances with corresponding points
     cout << "\n\nDistances from query point (" << query[0] << ", " << query[1] << "):\n";
     for (const auto &[point, distance] : distances) {
@@ -121,32 +87,36 @@ void print_results(const Dataset &dataset, const Data &query, const vector<Data>
 }
 
 // Checks the results of the Greedy search manually
-void check_results_manually(const Dataset &dataset, const Data &query, const Graph &result, int k, Dataset expected_neighbors_g) {
-    // Calculate the Euclidean distances of each point from the query
-    vector<pair<Data, data_t>> distances;
-    for (const auto &data : dataset) {
-        data_t dist = euclidean_distance(data, query);
-        distances.emplace_back(data, dist);
-    }
-    
-    // Sort distances in ascending order
-    sort(distances.begin(), distances.end(), [](const auto& a, const auto& b) {
-        return a.second < b.second;
-    });
+void check_results(const Dataset &dataset, const Data &query, const Graph &result, int k, vector<int> expected_neighbors_g) {
     
     // Get the expected nearest neighbors (if not provided)
-    vector<Data> expected_neighbors;
+    vector<Data> expected_neighbors; expected_neighbors.reserve(k);
     if (expected_neighbors_g.empty()) {
-        for (size_t i = 0; i < (size_t)k; i++) {
+        // Calculate the Euclidean distances of each point from the query
+        vector<pair<Data, euclidean_t>> distances; distances.reserve(dataset.size());
+        for (const auto &data : dataset) {
+            euclidean_t dist = euclidean_distance(data, query);
+            distances.emplace_back(data, dist);
+        }
+        
+        // Sort distances in ascending order
+        sort(distances.begin(), distances.end(), [](const auto& a, const auto& b) {
+            return a.second < b.second;
+        });
+
+        for (size_t i = 0; i < static_cast<size_t>(k); i++) {
             expected_neighbors.push_back(distances[i].first);
         }
     } else {
-        expected_neighbors = expected_neighbors_g;
+        // Here expected_neighbors_g is the groundtruth that contains the indices of the expected neighbors.
+        for (const auto &index : expected_neighbors_g) {
+            expected_neighbors.push_back(dataset[index]);
+        }
     }
 
     // Check if the number of neighbors found matches k
-    if (result.size() == (size_t)k) cout << " || Number of neighbors is k.\n";
-    else {cerr << " || Number of neighbors found is not k. Found: " << result.size() << " Expected: " << k << ".\n";}
+    if (static_cast<int>(result.size()) == k) cout << " || Size of neighbors found matches k.\n";
+    else {cerr << " || Size of neighbors found is not k. Found: " << result.size() << " Expected: " << k << ".\n";}
     int foundC = 0;
     
     // Check if the neighbor is in the expected neighbors
@@ -165,4 +135,5 @@ void check_results_manually(const Dataset &dataset, const Data &query, const Gra
         if (found) {foundC++;}
     }
     cout << " || Number of neighbors found in expected neighbors: " << foundC << ".\n";
+    cout << " || Recall@k: " << (double)(100*foundC/k) << "%." << endl;
 }
