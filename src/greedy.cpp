4b270be0cb77babd48275e922baf4e161d4e9250
Giorgos Ragkos <ragkos7@gmail.com>
2024-11-14 15:31:51 +0200

    start of greedy update

--- src/greedy.cpp
+++ src/greedy.cpp
@@ -16,14 +16,25 @@ void L_m_V(const vector<int> &L, const vector<int> &V, vector<int> &LV) {
 }
 
 // Greedy Algorithm - returning [k-nearest aprx. points, visited points]
-pair<vector<int>, vector<int>> greedy_search(const Graph &G, Graph_Node s, Data q, int k, int L_s){
+pair<vector<int>, vector<int>> greedy_search(const Graph &G, list<Graph_Node> S, Data q, int k, int L_s,float Filter){
 
-    // Initialize sets L<-{s}, V<-{}
-    vector<int> L; L.push_back(s->indx);
+    // Initialize sets L<-{}, V<-{}
+    vector<int> L; 
     vector<int> V;
 
+    //for s e S
+    for (auto s = S.begin(); s != S.end(); ++s) {
+            //If (Fs intersection Fx) != {}
+            if ((*s)->data[0] == Filter) {
+                //L <- L U {s}
+                L.push_back((*s)->indx);
+        }
+    }      
     // Initialize L \ V = {s}
-    vector<int> L_not_V = {s->indx};
+    vector<int> L_not_V;
+    for(auto s = S.begin(); s != S.end(); ++s){
+        L_not_V.push_back((*s)->indx);
+    }
     
     // While L \ V is not empty
     while (!L_not_V.empty()) {
@@ -31,14 +42,19 @@ pair<vector<int>, vector<int>> greedy_search(const Graph &G, Graph_Node s, Data 
         // Find p* <- argmin_{p \in L \ V} d(p,q)
         int p_star = find_min_dist(G, L_not_V, q);
 
-        // Update L <- L U N_out(p*), V <- V U {p*}
+        //V <- V U (p*)
+        V.push_back(p_star);
+
+        //Let N'_out(p*) <- p' ε N_out(p*) : (Fp' intersectiob Fq != {}), p' not belongs to V
+        unordered_set<int> N_; 
+
+        // Update L <- L U N'_out(p*)
         for (const auto &p : G[p_star]->out_neighbours) {
             if (find_if(L.begin(), L.end(), [p](int i) { return i == p; }) == L.end()) {
                 L.push_back(p);
             }
         }
-        V.push_back(p_star);
-
+        
         // If |L| > L_s then update L to retain closest L_s points to q
         if (L.size() > (size_t)L_s) {
             sort(L.begin(), L.end(), [&q, &G](int a, int b) {
