29a857a1a4dfc8527cd6c1cffbca08d48b14f5dd
Dimitris Dimochronis <dimitris.dimochronis@gmail.com>
2024-12-13 14:58:35 +0200

    Add option "q_idx=-3" to only index and not search

--- src/main.cpp
+++ src/main.cpp
@@ -19,7 +19,7 @@ int main(int argc, char *argv[]) {
 
     // Read the configuration file and set the parameters
     string dataset_f, query_f, groundtruth_f;
-    int k = 0, R = -1, L = -1, q_idx = -1;
+    int k = 0, R = -1, L = -1, q_idx = -3;
     double a = 0.0; string vam_func = "error";
     string graph_name = "graph.bin";
     bool print = false;
@@ -61,19 +61,22 @@ int main(int argc, char *argv[]) {
     if (vam_func == "vamana") {
         dataset = fvecs_read(dataset_f);
 
-        queries = fvecs_read(query_f);
-
-        groundtruth = ivecs_read(groundtruth_f);
+        if (q_idx != -3) {
+            queries = fvecs_read(query_f);
+            groundtruth = ivecs_read(groundtruth_f);
+        }
     } else {
         auto r = read_sigmod_dataset(dataset_f); 
         dataset = r.first; C = r.second;
 
-        auto r2 = read_sigmod_queries(query_f);
-        queries = r2.first; V = r2.second;
+        if (q_idx != -3) {
+            auto r2 = read_sigmod_queries(query_f);
+            queries = r2.first; V = r2.second;
 
-        groundtruth = read_sigmod_groundtruth(groundtruth_f);
-        if (groundtruth.empty()) groundtruth = find_store_groundtruth(r, r2, groundtruth_f);
-
+            groundtruth = read_sigmod_groundtruth(groundtruth_f);
+            if (groundtruth.empty()) groundtruth = find_store_groundtruth(r, r2, groundtruth_f);
+        }
+        
         set<int> C_set(C.begin(), C.end());
         for (int c : C_set) F.push_back(c);
     }
@@ -88,18 +91,20 @@ int main(int argc, char *argv[]) {
     
     // Set queries and groundtruth to test
     vector<int> indx_to_test;
-    if (q_idx == -1) { // Test all queries in the query file
-        for (int i = 0; i < (int)queries.size(); i++) indx_to_test.push_back(i);
+    if (q_idx != -3) {
+        if (q_idx == -1) { // Test all queries in the query file
+            for (int i = 0; i < (int)queries.size(); i++) indx_to_test.push_back(i);
 
-    } else if (q_idx == -2) { // Test a random query
-        q_idx = rand() % queries.size();
-        indx_to_test.push_back(q_idx);
+        } else if (q_idx == -2) { // Test a random query
+            q_idx = rand() % queries.size();
+            indx_to_test.push_back(q_idx);
 
-    } else { // Test a specific query
-        if (q_idx < 0 || q_idx >= (int)queries.size()) {
-            cerr << "Invalid query index, give [0, " << queries.size() - 1 << "]" << endl;
-            return 1;
-        } else indx_to_test.push_back(q_idx);
+        } else { // Test a specific query
+            if (q_idx < 0 || q_idx >= (int)queries.size()) {
+                cerr << "Invalid query index, give [0, " << queries.size() - 1 << "]" << endl;
+                return 1;
+            } else indx_to_test.push_back(q_idx);
+        }
     }
     
     cout << " || Dataset: " << dataset_f << endl;
@@ -134,76 +139,79 @@ int main(int argc, char *argv[]) {
     }
 
     time_elapsed(start, "Vamana Indexing");
-    cout << "=======================================================================================\n";
-    cout << "                                      Results:" << endl;
-    cout << "=======================================================================================\n";
-    
-    // Find the medoid for each label
-    unordered_map<int, int> medoid_map;
-    if (vam_func != "vamana") {
-        medoid_map.reserve(F.size());
-        medoid_map = find_medoid(dataset, C, 1, F);
-    }
-    
-    // Perform the Greedy search for each query
-    double recall_sum = 0.0; Graph G_q; int V_i;
-    for (const auto &i : indx_to_test) {
-        Data query = queries[i];
 
-        vector<int> groundtruth_t = groundtruth[i];
+    if (q_idx != -3) { // If user wants search
+        cout << "=======================================================================================\n";
+        cout << "                                      Results:" << endl;
+        cout << "=======================================================================================\n";
         
-        cout << " || Query: " << (q_idx != -1 ? q_idx : i);
+        // Find the medoid for each label
+        unordered_map<int, int> medoid_map;
         if (vam_func != "vamana") {
-            V_i = V[i];
-            cout << " | Label: " << V_i << endl;
-        } else cout << endl;
+            medoid_map.reserve(F.size());
+            medoid_map = find_medoid(dataset, C, 1, F);
+        }
+        
+        // Perform the Greedy search for each query
+        double recall_sum = 0.0; Graph G_q; int V_i;
+        for (const auto &i : indx_to_test) {
+            Data query = queries[i];
 
-        clock_t gr_start = clock();
+            vector<int> groundtruth_t = groundtruth[i];
+            
+            cout << " || Query: " << (q_idx != -1 ? q_idx : i);
+            if (vam_func != "vamana") {
+                V_i = V[i];
+                cout << " | Label: " << V_i << endl;
+            } else cout << endl;
 
-        // Perform greedy search starting based on the prefered function
-        pair<vector<int>, vector<int>> result_p;
+            clock_t gr_start = clock();
 
-        if (vam_func == "vamana") {
-            Graph_Node s = G.front();
+            // Perform greedy search starting based on the prefered function
+            pair<vector<int>, vector<int>> result_p;
 
-            result_p = greedy_search(G, s, query, k, L);
+            if (vam_func == "vamana") {
+                Graph_Node s = G.front();
 
-        } else {
-            vector<int> fq, S;
-
-            if (V_i == -1) { // Search for classic ANN
-                // Set medoid for each label the result of greedy search with k = 1 for the subgraph of the every label
-                for (const auto &f : F) {
-                    auto result_l = filtered_greedy_search(G, {medoid_map[f]}, query, 1, L, C, {f});
-                    S.push_back(result_l.first.front());
-                }
-                fq = F;
+                result_p = greedy_search(G, s, query, k, L);
 
             } else {
-                S.push_back(medoid_map[V_i]);
-                fq = {V_i};
+                vector<int> fq, S;
+
+                if (V_i == -1) { // Search for classic ANN
+                    // Set medoid for each label the result of greedy search with k = 1 for the subgraph of the every label
+                    for (const auto &f : F) {
+                        auto result_l = filtered_greedy_search(G, {medoid_map[f]}, query, 1, L, C, {f});
+                        S.push_back(result_l.first.front());
+                    }
+                    fq = F;
+
+                } else {
+                    S.push_back(medoid_map[V_i]);
+                    fq = {V_i};
+                }
+
+                result_p = filtered_greedy_search(G, S, query, k, L, C, fq);
+
             }
 
-            result_p = filtered_greedy_search(G, S, query, k, L, C, fq);
+            auto result = result_p.first; auto visited = result_p.second;
 
+            // Print time for each Greedy call
+            if (print) time_elapsed(gr_start, "Greedy Search " + to_string(i + 1) + "/" + to_string(indx_to_test.size()));
+            
+            // Print the results
+            recall_sum += check_results(dataset, query, result, k, groundtruth_t, true);
         }
-
-        auto result = result_p.first; auto visited = result_p.second;
-
-        // Print time for each Greedy call
-        if (print) time_elapsed(gr_start, "Greedy Search " + to_string(i + 1) + "/" + to_string(indx_to_test.size()));
         
-        // Print the results
-        recall_sum += check_results(dataset, query, result, k, groundtruth_t, true);
-    }
-    
-    // Print time for both Vamana and Greedy calls
-    time_elapsed(start, "both Vamana and Greedy calls");
+        // Print time for both Vamana and Greedy calls
+        time_elapsed(start, "both Vamana and Greedy calls");
 
-    // Print the average recall
-    cout << "=======================================================================================\n";
-    cout << "===> Average Recall@" << k << ": " << (double)(recall_sum/indx_to_test.size()) << "%" << endl;
-    cout << "=======================================================================================\n";
+        // Print the average recall
+        cout << "=======================================================================================\n";
+        cout << "===> Average Recall@" << k << ": " << (double)(recall_sum/indx_to_test.size()) << "%" << endl;
+        cout << "=======================================================================================\n";
+    }
 
     // Free the memory allocated for the graph
     for (const auto &node : G) delete node;
